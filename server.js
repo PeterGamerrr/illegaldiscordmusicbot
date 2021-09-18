47d52ee76d0a029679c755d70c7c3d0fa1b91413
PeterGamerrr <peterrr@assenbergh.nl>
2021-09-18 17:11:05 +0200

    added listeners

--- server.js
+++ server.js
@@ -4,3 +4,24 @@ const {
 	token,
 } = require('./config.json');
 const ytdl = require('ytdl-core');
+
+
+const client = new Discord.Client();
+
+
+
+
+client.once('ready', () => {
+    console.log('Ready!');
+});
+
+client.once('reconnecting', () => {
+    console.log('Reconnecting!');
+});
+
+client.once('disconnect', () => {
+    console.log('Disconnect!');
+});
+
+
+client.login(token);
